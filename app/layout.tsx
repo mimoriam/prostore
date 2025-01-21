f04dbb2a003cdda2f03f257b45a056a60ea9f373
Mimo <mimoriam420@gmail.com>
2025-01-22 03:36:59 +0500

    refactor(app): constants & file layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,19 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+
 import "@/assets/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pro-store",
-  description: "A modern store built with Next.js, ShadCN, and Prisma.",
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
