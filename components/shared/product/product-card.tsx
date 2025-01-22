228b2ed127f406d5fdfbbe984af68307cda0d89a
Mimo <mimoriam420@gmail.com>
2025-01-22 18:17:44 +0500

    feat(app): product price

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import ProductPrice from "@/components/shared/product/product-price";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 const ProductCard = ({ product }: { product: any }) => {
@@ -29,7 +30,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out of Stock</p>
           )}
