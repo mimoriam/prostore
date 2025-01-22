bcc59781b9528641eb529247e9b5a5d4c3c76a1c
Mimo <mimoriam420@gmail.com>
2025-01-22 18:11:17 +0500

    feat(app): product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
+import ProductList from "@/components/shared/product/product-list";
+
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
   return (
-    <main>
-      <div>Products</div>
-    </main>
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+
+      <ProductList title="Newest Arrivals" data={sampleData.products} limit={4} />
+    </div>
   );
 };
 
