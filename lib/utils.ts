66ceae3a5a739e8e78b816a4d1c0d8939ea2430b
Mimo <mimoriam420@gmail.com>
2025-01-24 23:05:41 +0500

    feat(app): zod for showing products 1/2

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Format number with decimal places
+export const currency = z
+  .string()
+  .refine(
+    (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+    "Price must have exactly two decimal places (e.g., 49.99)",
+  );
+
+// This function ensures numbers always have two decimal places. For example:
+// 49 becomes “49.00”.
+// 49.9 becomes “49.90”.
+// This is important for monetary values where precision matters.
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
