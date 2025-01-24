e7dceaa23e0f64ef98db2ce9ad178b539fddf949
Mimo <mimoriam420@gmail.com>
2025-01-24 23:23:34 +0500

    feat(app): product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,14 +4,20 @@ import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
 
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 // Get the latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return data;
+}
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+  return prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
